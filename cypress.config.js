07e72385143bfe28672a1dc22ac429959872db3f
Gutenberg <guto.the.big@gmail.com>
2024-05-11 00:01:14 -0300

    ci/cd

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+
+  viewportWidth: 1280,
+  viewportHeight: 880,
+  video: false,
+  
   e2e: {
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
